0608eefa08d330386e405e462753e5cce9ffbf4e
Hacerx <pepesoler98@gmail.com>
2018-12-11 23:15:55 +0100

    Add files via upload

--- array_posibilidades.cc
+++ array_posibilidades.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -12,6 +13,13 @@ using namespace std;
 ###    ###       ###     ###    ########       ##########       ###    ###      ###    ###
 */
 
+// calculo de la binomial para anticipar el resultado
+unsigned C(unsigned n, unsigned k) {
+    if (n == k || k == 0) {
+        return 1;
+    }
+    return C(n - 1, k - 1) * n / k;
+}
 
 // Suma 1 en binario
 int *sumaBinario(int num[], int size){
@@ -37,9 +45,10 @@ void mostrar(int num[], int size){
 
 int main(){
   // Puedes modificar
-  size_t dimension = 5;
+  size_t dimension = 25;
+  int unos = 3;
   // elige si quieres unos
-  bool cantidadConcretaDeUnos = false;
+  bool cantidadConcretaDeUnos = true;
   // elige si quieres mostrar las posibilidades
   bool mostrarPosibilidades = false;
   // a partir de aquí no
@@ -49,11 +58,19 @@ int main(){
   for(int i = 0; i < dimension; i++){
     posibilidades[i] = 0;
   }
-  int unos = 3;
   int count = 0;
+  int resultado;
+  if(cantidadConcretaDeUnos){
+    resultado = C(dimension, unos);
+  }
+  unsigned t0, t1;
+  t0 = clock();
   while(!fin){
     sumaBinario(posibilidades, dimension);
     if(cantidadConcretaDeUnos){
+      if(resultado == count){
+        break;
+      }
       int aux = 0;
       // si quisieras comprobar un número limitado de unos
       for(int i=0; i < dimension; i++){
@@ -76,12 +93,16 @@ int main(){
     }
     fin = true;
     // condicion de parada, si todos son 1s para
-    for(int i=0; i < dimension; i++){
+    for(int i=0; i < dimension || fin; i++){
       if(posibilidades[i] == 0){
         fin = false;
       }
     }
 
   }
+  t1 = clock();
+  double time = (double(t1-t0)/1000);
+  cout << "Execution Time: " << time << endl;
+  //cout << C(dimension, unos) << endl;
   cout << "Casos a evaluar "<< count << endl;
 }
